ca83017e6b65ed6cd40576e0c24731ffb52e8aed
Carlitoleroi <cgauguin@savorygourmet.com>
2026-08-30 21:56:24 -0700

    Change Supabase URL in config.js
    Updated the Supabase URL in the configuration.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  supabaseUrl: 'https://wluqovwpfqonmvmzjcfd.supabase.co',
+  supabaseUrl: 'wluqoywpfqonmvmzjcfd.supabase.co',,
   supabaseAnonKey: 'sb_publishable_eZXZsiH3JP0-Di712fGQyg_DH5V5oRN',
   companyName: 'Team Savory',
   appName: 'Sales Visits',
